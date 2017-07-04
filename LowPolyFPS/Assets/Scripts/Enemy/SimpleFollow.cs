33fd966dcaca3f6861cf709c08afb21ec6b5bfae
Evan <daley.evan@gmail.com>
2017-07-04 08:22:51 -0700

    Started working on enemy AI

---
+++ LowPolyFPS/Assets/Scripts/Enemy/SimpleFollow.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class SimpleFollow : MonoBehaviour {
+
+	protected NavMeshAgent navMeshAgent;
+	public Transform target;
+
+	void Start () 
+	{
+		navMeshAgent = GetComponent<NavMeshAgent> ();
+
+		InvokeRepeating ("UpdateTarget", 3f, 3f);
+	}
+
+	void UpdateTarget()
+	{
+		navMeshAgent.SetDestination (target.position);
+	}
+	
+	void Update () 
+	{
+		
+	}
+}
